4a83046efa3f390ea5c4811076eceff9c11e24eb
Mauro-27 <mauroxcampos.27@gmail.com>
2023-07-26 19:42:16 -0300

    Primer commit de MercadoLiebre

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/",(req, res) => res.sendFile(path.join(__dirname, "views", "home.html")))
+
+
+app.get("/login",(req,res) => {
+    res.sendFile(__dirname + "/views/login.html")
+})
+
+
+app.get("/register",(req,res) => {
+    res.sendFile(__dirname + "/views/register.html")
+})
+
+
+
+app.listen(PORT, () => console.log("Servidor corriendo en el puerto: " + PORT))
